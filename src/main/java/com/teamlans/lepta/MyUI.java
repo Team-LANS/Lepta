69c67554d7cb5fde9ac4c53daf82b0b934c09a88
hschroedl <hansj.schroedl@gmail.com>
2016-07-16 22:30:43 +0200

    Start creating base page layout [#4]

--- src/main/java/com/teamlans/lepta/MyUI.java
+++ src/main/java/com/teamlans/lepta/MyUI.java
@@ -1,5 +1,6 @@
 package com.teamlans.lepta;
 
+import com.teamlans.lepta.view.MainView;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
@@ -19,21 +20,7 @@ import javax.servlet.annotation.WebServlet;
 @Theme("mytheme") @Widgetset("com.teamlans.lepta.MyAppWidgetset") public class MyUI extends UI {
 
   @Override protected void init(VaadinRequest vaadinRequest) {
-    final VerticalLayout layout = new VerticalLayout();
-
-    final TextField name = new TextField();
-    name.setCaption("Type your name here:");
-
-    Button button = new Button("Click Me");
-    button.addClickListener(e -> {
-      layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
-    });
-
-    layout.addComponents(name, button);
-    layout.setMargin(true);
-    layout.setSpacing(true);
-
-    setContent(layout);
+    setContent(new MainView());
   }
 
   @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
