9c1a791db16fd96ea45400ca69ce5c3fc3ecce15
hschroedl <hansj.schroedl@gmail.com>
2016-07-23 11:45:49 +0200

    Update layout for create new bill [#4]

--- src/main/java/com/teamlans/lepta/view/bills/create/ManualCreateBillViewImpl.java
+++ src/main/java/com/teamlans/lepta/view/bills/create/ManualCreateBillViewImpl.java
@@ -12,6 +12,7 @@ import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.Page;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 
@@ -26,6 +27,7 @@ import javax.annotation.PostConstruct;
   private UserDao userDao;
   private TextField nameField;
   private DateField dateField;
+  private Table itemTable;
 
   @Autowired public void setUserDao(UserDao userDao) {
     this.userDao = userDao;
@@ -39,32 +41,84 @@ import javax.annotation.PostConstruct;
     setMargin(false);
     setSpacing(true);
     VerticalLayout layout = buildRootLayout();
+    layout.setResponsive(true);
     addComponent(layout);
   }
 
   private VerticalLayout buildRootLayout() {
     VerticalLayout layout = new VerticalLayout();
-    Label label = new Label("Create new bill");
-    layout.addComponent(label);
-    createNameField(layout);
-    dateField = new DateField();
-    layout.addComponent(dateField);
-    Button button = new Button("Create");
-    button.addClickListener(event -> tryAddBill());
-    layout.addComponent(button);
-    Button cancel = new Button("Cancel");
-    layout.addComponent(cancel);
+    layout.setSizeFull();
+    createGridLayout(layout);
     return layout;
   }
 
-  private void createNameField(VerticalLayout layout) {
-    nameField = new TextField("Enter name here");
+  private void createGridLayout(VerticalLayout layout) {
+    GridLayout gridLayout = new GridLayout(2, 3);
+    gridLayout.setWidth("600px");
+    gridLayout.setHeight("200px");
+    gridLayout.setRowExpandRatio(1,1);
+    createHeaderLabel(gridLayout);
+    createInputFields(gridLayout);
+    createItemList(gridLayout);
+    createButtonBar(gridLayout);
+    layout.addComponent(gridLayout);
+    layout.setComponentAlignment(gridLayout, Alignment.MIDDLE_CENTER);
+  }
+
+  private void createInputFields(GridLayout gridLayout) {
+    VerticalLayout inputContainer = new VerticalLayout();
+    inputContainer.setSpacing(true);
+    dateField = new DateField();
+    dateField.setDateFormat("yyyy-MM-dd");
+    inputContainer.addComponent(dateField);
+    nameField = new TextField();
+    nameField.setInputPrompt("Enter bill name");
     nameField.addValidator(new StringLengthValidator("Name must not be empty.", 1, 50, false));
     nameField.setNullRepresentation("");
     nameField.setValidationVisible(false);
-    layout.addComponent(nameField);
+    inputContainer.addComponent(nameField);
+    gridLayout.addComponent(inputContainer, 0, 1);
   }
 
+  private void createItemList(GridLayout gridLayout) {
+    VerticalLayout listContainer = new VerticalLayout();
+    listContainer.setSpacing(true);
+    itemTable = new Table();
+    itemTable.setHeight("300px");
+    itemTable.addContainerProperty("Name", String.class, null);
+    itemTable.addContainerProperty("Price", Double.class, null);
+    itemTable.setWidth("100%");
+    listContainer.addComponent(itemTable);
+    listContainer.setExpandRatio(itemTable, 1);
+    createItemInput(listContainer);
+    gridLayout.addComponent(listContainer, 1, 1);
+  }
+
+  private void createItemInput(VerticalLayout listContainer) {
+    HorizontalLayout itemInputContainer = new HorizontalLayout();
+    itemInputContainer.setSpacing(true);
+    TextField itemName = new TextField();
+    itemInputContainer.addComponent(itemName);
+    TextField itemPrice = new TextField();
+    itemInputContainer.addComponent(itemPrice);
+    Button addItem = new Button("Add");
+    itemInputContainer.addComponent(addItem);
+    listContainer.addComponent(itemInputContainer);
+  }
+
+  private void createHeaderLabel(GridLayout layout) {
+    Label label = new Label("Create new bill");
+    label.setStyleName(ValoTheme.LABEL_H1);
+    layout.addComponent(label, 0, 0, 1, 0);
+  }
+
+  private void createButtonBar(GridLayout layout) {
+    Button cancelButton = new Button("Cancel");
+    layout.addComponent(cancelButton, 0, 2);
+    Button okayButton = new Button("Okay");
+    layout.addComponent(okayButton, 1, 2);
+    layout.setComponentAlignment(okayButton, Alignment.MIDDLE_RIGHT);
+  }
 
   private void tryAddBill() {
     nameField.setValidationVisible(false);
