1f556242360c4239c59ef7f3e96fa3964ec5f3cd
Mailea <lena.schnedlitz@gmx.at>
2016-07-24 11:56:04 +0200

    issue #2: user login

--- src/main/java/com/teamlans/lepta/view/login/LoginView.java
+++ src/main/java/com/teamlans/lepta/view/login/LoginView.java
@@ -5,46 +5,73 @@ import com.teamlans.lepta.service.exceptions.LeptaLoginException;
 import com.teamlans.lepta.service.exceptions.LeptaServiceException;
 import com.teamlans.lepta.service.user.UserService;
 import com.teamlans.lepta.view.component.forms.LeptaLoginForm;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 
+
+/**
+ * Custom login layout. Credentials can be entered into a LeptaLoginForm and are handled by a
+ * UserService.
+ */
 public class LoginView extends VerticalLayout {
 
   private UserService userService;
 
   public LoginView() {
+    userService = new UserService();
     build();
   }
 
   private void build() {
     setSizeFull();
 
-    final VerticalLayout center = new VerticalLayout();
+    final VerticalLayout center = createContainerWithTitle("WELCOME");
     addComponent(center);
     setComponentAlignment(center, Alignment.MIDDLE_CENTER);
 
-    Label welcome = new Label("WELCOME");
-    welcome.setSizeUndefined();
-    center.addComponent(welcome);
-    center.setComponentAlignment(welcome, Alignment.TOP_CENTER);
+    final LeptaLoginForm loginForm = createLoginForm();
+    center.addComponent(loginForm);
+    center.setComponentAlignment(loginForm, Alignment.MIDDLE_CENTER);
+  }
 
-    userService = new UserService();
+  private VerticalLayout createContainerWithTitle(String text) {
+    final VerticalLayout container = new VerticalLayout();
+
+    final Label title = new Label(text);
+    title.setSizeUndefined();
+    container.addComponent(title);
+    container.setComponentAlignment(title, Alignment.TOP_CENTER);
+
+    return container;
+  }
+
+  private LeptaLoginForm createLoginForm() {
     final LeptaLoginForm loginForm = new LeptaLoginForm();
     loginForm.addLoginListener(new LoginForm.LoginListener() {
       @Override
       public void onLogin(LoginForm.LoginEvent event) {
         try {
           userService.authenticate(event.getUserName(), event.getPassword());
+          // TODO: load content
         } catch (LeptaLoginException e) {
-          // TODO
+          showNotification("Login failed", "This username and password combination does not " +
+              "exist.\nPlease try again.");
         } catch (LeptaServiceException e) {
-          // TODO
+          showNotification("Login failed", "Something went wrong.\nPlease try again.");
         }
       }
     });
-    center.addComponent(loginForm);
-    center.setComponentAlignment(loginForm, Alignment.MIDDLE_CENTER);
+    return loginForm;
+  }
+
+  private void showNotification(String caption, String description) {
+    Notification notification = new Notification(caption, description);
+    notification.setPosition(Position.BOTTOM_CENTER);
+    notification.setStyleName(ValoTheme.NOTIFICATION_ERROR + " " + ValoTheme.NOTIFICATION_CLOSABLE);
+    notification.setDelayMsec(5000);
+    notification.show(Page.getCurrent());
   }
 
 }
