5888fda8f4407dbe139fbad45a0f874ec06df63e
hschroedl <hansj.schroedl@gmail.com>
2016-07-28 10:48:15 +0200

    Improve Item table

--- src/main/java/com/teamlans/lepta/view/bill/edit/component/items/ItemTable.java
+++ src/main/java/com/teamlans/lepta/view/bill/edit/component/items/ItemTable.java
@@ -1,24 +1,28 @@
 package com.teamlans.lepta.view.bill.edit.component.items;
 
 import com.teamlans.lepta.entities.Item;
+import com.vaadin.data.Container;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.DefaultFieldFactory;
+import com.vaadin.ui.Field;
+import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.TableFieldFactory;
+import com.vaadin.ui.TextField;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ItemTable extends Table {
+public class ItemTable extends ListSelect {
 
   private BeanItemContainer<Item> itemContainer;
 
   public ItemTable() {
-    setImmediate(true);
     itemContainer = new BeanItemContainer<>(Item.class);
     itemContainer.addAll(new ArrayList<>());
     setContainerDataSource(itemContainer);
-    addGeneratedColumn("test", new DeleteButtonGenerator());
-    setVisibleColumns("name", "price", "test");
   }
 
   public List<Item> getItems() {
@@ -33,28 +37,50 @@ public class ItemTable extends Table {
     itemContainer.addItem(item);
   }
 
-  private class DeleteButtonGenerator implements ColumnGenerator {
+  private class DeleteButtonGenerator implements Table.ColumnGenerator {
     @Override
     public Object generateCell(Table table, Object itemId, Object columnId) {
-      Button button = new Button(itemId == table.getData() ? "Save" : "Edit");
-      button.addClickListener((Button.ClickListener) event -> {
-        if (table.getData() == null) {
-          table.setData(itemId);
-          table.refreshRowCache();
-        } else if (itemId == table.getData()) {
-          table.setData(null);
-          table.refreshRowCache();
-        }
-      });
+      setData(itemId);
+      Button button = new Button("X");
+      button.addClickListener(clickEvent -> table.removeItem(getData()));
       return button;
     }
   }
 
-  private class EditButtonGenerator implements ColumnGenerator {
+  private class EditButtonGenerator implements Table.ColumnGenerator {
     @Override
     public Object generateCell(Table table, Object itemId, Object columnId) {
-      Button button = new Button(itemId == table.getData() ? "Save" : "Edit");
+      Button button = new Button(itemId == getData() ? "Save" : "Edit");
+      button.addClickListener(event -> editTable(event));
       return button;
     }
   }
+
+  private class CustomFieldFactory implements TableFieldFactory {
+    @Override
+    public Field<?> createField(Container container, Object itemId, Object propertyId, Component uiContext) {
+      if (itemId == getData()) {
+        return DefaultFieldFactory.get().createField(container, itemId, propertyId, uiContext);
+      }
+      return null;
+    }
+  }
+
+  private void editTable(final Button.ClickEvent ce) {
+    setTableFieldFactory(new TableFieldFactory() {
+      @Override
+      public Field createField(Container container, Object itemId, Object propertyId, Component uiContext) {
+          if (propertyId.toString().equals("expDir")) {
+            return new TextField();
+          }
+          if (propertyId.toString().equals("participant")) {
+            return new TextField();
+          }
+        return null;
+      }
+    });
+    setEditable(true);
+  }
+
+
 }
