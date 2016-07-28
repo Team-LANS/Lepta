a77522ed5b5c5c756fc7ccac8206f408fa2aed71
hschroedl <hansj.schroedl@gmail.com>
2016-07-28 17:24:18 +0200

    Improve bill overview look [#5]

--- src/main/java/com/teamlans/lepta/view/bill/NewBillsView.java
+++ src/main/java/com/teamlans/lepta/view/bill/NewBillsView.java
@@ -8,8 +8,11 @@ import com.teamlans.lepta.view.bill.edit.EditBillView;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.VerticalLayout;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -27,22 +30,52 @@ public class NewBillsView extends ProtectedVerticalView {
 
   private BeanItemContainer<Bill> container;
 
+  private Button editButton;
+
+  private Button deleteButton;
+
   @PostConstruct
   void init() {
     setSpacing(true);
-    addComponent(buildBillsTable());
-    addComponent(buildAddBillButton());
-    addComponent(buildEditBillButton());
-    addComponent(buildDeleteBillButton());
+    setMargin(true);
+    VerticalLayout centerContainer = new VerticalLayout();
+    centerContainer.setSpacing(true);
+    centerContainer.setWidth("70%");
+    centerContainer.addComponent(buildBillsTable());
+    HorizontalLayout buttonContainer = buildButtonContainer();
+    centerContainer.addComponent(buttonContainer);
+    addComponent(centerContainer);
+    setComponentAlignment(centerContainer, Alignment.MIDDLE_CENTER);
+  }
+
+  private HorizontalLayout buildButtonContainer() {
+    HorizontalLayout buttonContainer = new HorizontalLayout();
+    buttonContainer.setSizeFull();
+    buttonContainer.setSpacing(true);
+    buttonContainer.addComponent(buildAddBillButton());
+    editButton = buildEditBillButton();
+    buttonContainer.addComponent(editButton);
+    buttonContainer.setComponentAlignment(editButton, Alignment.MIDDLE_CENTER);
+    editButton.setEnabled(false);
+    deleteButton = buildDeleteBillButton();
+    buttonContainer.addComponent(deleteButton);
+    buttonContainer.setComponentAlignment(deleteButton, Alignment.MIDDLE_RIGHT);
+    deleteButton.setEnabled(false);
+    return buttonContainer;
   }
 
   private Table buildBillsTable() {
     table = new Table();
+    table.setSizeFull();
     container = new BeanItemContainer<>(Bill.class);
     container.addAll(billService.listBillsFor(getLeptaUi().getLoggedInUser()));
     table.setContainerDataSource(container);
     table.setVisibleColumns("name");
     table.setSelectable(true);
+    table.addItemClickListener(event -> {
+      editButton.setEnabled(true);
+      deleteButton.setEnabled(true);
+    });
     return table;
   }
 
